9feb42bdccc1b6b8a8bdf353b8cc43f798b80682
Nahidul Islam <nahiditclanbd@gmail.com>
2024-02-25 18:01:07 +0600

    create vehicle controller, request, resource

---
+++ app/Http/Controllers/Api/V1/VehicleController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Vehicle;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\VehicleResource;
+use App\Http\Requests\VehicleStoreRequest;
+
+class VehicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return VehicleResource::collection(Vehicle::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(VehicleStoreRequest $request)
+    {
+        $vehicle = Vehicle::create($request->validated());
+
+        return VehicleResource::make($vehicle);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Vehicle $vehicle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Vehicle $vehicle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Vehicle $vehicle)
+    {
+        //
+    }
+}

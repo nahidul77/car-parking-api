a99019b2ca7ca8f0e25a665abe02ce4e81100d28
Nahidul Islam <nahiditclanbd@gmail.com>
2024-02-25 17:32:34 +0600

    Profile show, update & password update

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Controllers\Api\V1\Auth\LoginController;
-use App\Http\Controllers\Api\V1\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\Auth\LoginController;
+use App\Http\Controllers\Api\V1\Auth\LogoutController;
+use App\Http\Controllers\Api\V1\Auth\ProfileController;
+use App\Http\Controllers\Api\V1\Auth\RegisterController;
+use App\Http\Controllers\Api\V1\Auth\PasswordUpdateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +19,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('auth/logout', LogoutController::class);
+
+    Route::get('profile', [ProfileController::class, 'show']);
+    Route::put('profile', [ProfileController::class, 'update']);
+    Route::put('password', PasswordUpdateController::class);
 });
 
 Route::post('auth/register', RegisterController::class);
